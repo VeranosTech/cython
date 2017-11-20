34006f034d5a0ba5e79f1f00da4265abb686e9fe
joelkim <kim.dohhyoung@gmail.com>
2017-11-20 12:31:09 +0900

    add github page creation script

---
+++ create_ghpages.sh
@@ -0,0 +1,14 @@
+
+git checkout -B gh-pages
+git rebase docs-korean
+touch .nojekyll
+echo '!docs/build/' >> .gitignore
+cd docs
+make html
+cd ..
+git add . -A
+git commit -m "build"
+git push -f origin gh-pages
+git reset --hard HEAD
+git clean -f
+git checkout docs-korean
